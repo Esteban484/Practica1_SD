de34480ec7f2c658ddf9d8193401029451065b22
Esteban Samaniego <67563252+Esteban484@users.noreply.github.com>
2022-04-12 11:21:39 -0500

    Actualizando cliente_Multiuser

--- Ejercicio3_Multiuser/cliente.py
+++ Ejercicio3_Multiuser/cliente.py
@@ -1,16 +1,28 @@
 import socket
-ClientMultiSocket = socket.socket()
-host = '172.17.37.36'
-port = 2004
-print('Waiting for connection response')
-try:
-    ClientMultiSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
-res = ClientMultiSocket.recv(1024)
+
+host = "172.17.37.152"
+port = 1235
+
+sock = socket.socket()
+
+sock.connect((host, port))
+
+datos = sock.recv(4096)
+print (datos.decode('utf-8'))
+
+
+
 while True:
-    Input = input('Hey there: ')
-    ClientMultiSocket.send(str.encode(Input))
-    res = ClientMultiSocket.recv(1024)
-    print(res.decode('utf-8'))
-ClientMultiSocket.close()
+
+
+  message = input("digita un mensaje: ")
+  sock.send(message.encode('utf-8'))
+
+
+
+
+  if message == "salir":
+        print("Adios")
+        
+        sock.close()
+        break
