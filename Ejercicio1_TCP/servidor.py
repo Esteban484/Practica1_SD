bb36a05f5fff971ff999feff8705d6ece860dbb7
Esteban Samaniego <67563252+Esteban484@users.noreply.github.com>
2022-04-12 11:28:09 -0500

    actuliazndo servidor:TCP

--- Ejercicio1_TCP/servidor.py
+++ Ejercicio1_TCP/servidor.py
@@ -1,15 +1,29 @@
-import socket
-HOST=""
-PORT=14256
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    print("esperando conexion")
-    s.bind((HOST,PORT))
-    s.listen()
-    conn,addr=s.accept()
-    with conn:
-        print(f"Conectado al cliente{addr}")
-        while True:
-            data=conn.recv(1024)
-            if not data:
+from socket import *
+
+
+HOST = ''
+PORT = 1234
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)#Inicialización
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen()# Puerto de escucha
+
+
+while True:
+    print('esperando conexion')#Esperando la conexión del cliente
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('conectado desde:{}'.format(addr))
+
+    while True:
+        data = tcpCliSock.recv(BUFSIZ).decode()# Personajes aceptados
+        if data:
+                print('recibido: {}'.format(data.encode('utf-8')))
+        else:
+                print("Sin conexion de cliente")
                 break
-            conn.sendall(data)
+        mensaje = "Hola, te saluda el servidor"
+        tcpCliSock.send(mensaje.encode())# Enviar personaje
+    tcpCliSock.close()
+tcpSerSock.close()#Cerrar puerto
